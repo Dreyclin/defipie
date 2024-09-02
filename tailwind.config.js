8687b6bd51f09cf1211b5ffc7648023d123fcb9a
Maksym <makarka16@gmail.com>
2024-09-02 16:10:11 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js,css}"],
+  theme: {
+    fontFamily: {
+      'popins': ['Poppins', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
